8e2ee24170e44fc3f8aa32278ff48ebd1ca76017
pinetree2 <pinetree3966@gmail.com>
2024-02-01 02:52:27 +0000

    feat: state

--- stage/services/webserver-cluster/stage/data-stores/mysql/variable.tf
+++ stage/services/webserver-cluster/stage/data-stores/mysql/variable.tf
@@ -2,13 +2,13 @@ variable "db_username" {
  description = "The username for the database"
  type = string
  sensitive = true
- default = "test1234"
+
 }
 
 variable "db_password" {
  description = "The password for the database"
  type = string
  sensitive = true
- default = "test1234"
+
 
 }
